8c48b848a02849273b6b6c01e40b0b79522a484a
Amir Hitavi <111892729+AmirHitavi@users.noreply.github.com>
2023-07-26 07:56:00 +0330

    Update helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -6,7 +6,7 @@ const networkConfig = {
         entranceFee: ethers.utils.parseEther("0.01"),
         VRFCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
         keyHash: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
-        subscriptionId: "3882",
+        subscriptionId: your subscription Id, //"3882"
         callbackGasLimit: "500000",
         interval: "30",
     },
